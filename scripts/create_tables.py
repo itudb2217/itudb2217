a543515fb6c399e28193a37d972a4f65d72b4d7c
Enes1747 <enesyarica@gmail.com>
2022-12-03 16:26:43 +0300

    table statements update1

---
+++ scripts/create_tables.py
@@ -0,0 +1,115 @@
+import sqlite3 as dbapi2
+
+INIT_STATEMENTS = [
+    """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS teams (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        teamName TEXT NOT NULL,
+        playOff BOOL NOT NULL,
+        numOfGames INTEGER NOT NULL,
+        matchPoints INTEGER NOT NULL,
+        fieldGoals INTEGER NOT NULL,
+        percentageFG FLOAT NOT NULL,
+        seasonID INTEGER NOT NULL,
+        FOREIGN KEY (seasonID) REFERENCES ENDOFSEASONS(id) ON DELETE CASCADE ON UPDATE CASCADE
+
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS players (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        playerName TEXT NOT NULL,
+        position TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        leauge TEXT NOT NULL,
+        experince TEXT NOT NULL,
+        teamID INTEGER NOT NULL,
+        FOREIGN KEY (teamID) REFERENCES TEAMS(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        seasonID INTEGER NOT NULL,
+        FOREIGN KEY (seasonID) REFERENCES ENDOFSEASONS(id) ON DELETE CASCADE ON UPDATE CASCADE
+
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS games (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        gameDate TEXT NOT NULL,
+        gameStatus TEXT NOT NULL,
+        homeTeamPoints INTEGER NOT NULL,
+        percentageFG INTEGER NOT NULL,
+        percentageFT INTEGER NOT NULL,
+        homeTeamID INTEGER NOT NULL,
+        FOREIGN KEY (homeTeamID) REFERENCES TEAMS(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        visitorTeamID INTEGER NOT NULL,
+        FOREIGN KEY (visitorTeamID) REFERENCES TEAMS(id) ON DELETE CASCADE ON UPDATE CASCADE
+
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS stats (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        teamName TEXT NOT NULL,
+        loss BOOL NOT NULL,
+        win BOOL NOT NULL,
+        age INTEGER NOT NULL,
+        winPercentage INTEGER NOT NULL,
+        conference TEXT NOT NULL,
+        abbreviation INTEGER NOT NULL,
+        seasonID INTEGER NOT NULL,
+        FOREIGN KEY (seasonID) REFERENCES ENDOFSEASONS(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        playerID INTEGER NOT NULL,
+        FOREIGN KEY (seasonID) REFERENCES PLAYERS(id) ON DELETE CASCADE ON UPDATE CASCADE
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS game_detail (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        comment TEXT NOT NULL,
+        minute INTEGER NOT NULL,
+        nickName TEXT NOT NULL,
+        city TEXT NOT NULL,
+        startingPosition TEXT NOT NULL,
+        gameID INTEGER NOT NULL,
+        FOREIGN KEY (gameID) REFERENCES GAMES(id) ON DELETE CASCADE ON UPDATE CASCADE
+
+
+
+        
+    )
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS endofseasons (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT NOT NULL,
+        teamNo TEXT NOT NULL,
+        position TEXT NOT NULL,
+        voteNo INTEGER NOT NULL,
+        birth TEXT NOT NULL,
+        season INTEGER NOT NULL,
+        playerID INTEGER NOT NULL,
+        FOREIGN KEY (playerID) REFERENCES PLAYERS(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        teamID INTEGER NOT NULL,
+        FOREIGN KEY (teamID) REFERENCES TEAMS(id) ON DELETE CASCADE ON UPDATE CASCADE
+    )
+    """,
+]
+# enofseason içinde non key olan season ve birth revize edilebilir ekstradan
+
+
+def create_tables():
+    with dbapi2.connect("testing.db") as connection:
+        cursor = connection.cursor()
+        for statement in INIT_STATEMENTS:
+            # print(statement)
+            cursor.execute(statement)
+        connection.commit()
+
+
+create_tables()
