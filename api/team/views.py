1f2d43e181e95ec9425959d4b92989347530721a
gokaysari <60223343+gokaysari@users.noreply.github.com>
2022-12-11 16:20:01 +0300

    player and team functions added

--- api/team/views.py
+++ api/team/views.py
@@ -1,3 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from dbops.team import Team
 
-api = Blueprint("stats_api", __name__)
+api = Blueprint("team_api", __name__)
+
+
+@api.route("/get/<id>", methods=["GET", "POST"])
+def get(id):
+    user = Team.get_by_id(id)
+    if not user:
+        return jsonify({"status": "fail", "message": "user does not exist"})
+    return jsonify({"status": "success", "data": user})
+
+    # html 
+
+
+@api.route("/create", methods=["POST", "GET"])
+def createTeam():
+    team = Team.create("teamData")
+    return True
+
+
+@api.route("/get/all", methods=["GET", "POST"])
+def get_all():
+    data = Team.get_all()
+    print(data)
+    return jsonify({"status": "success", "data": data})
+
+
+@api.route("/delete/<id>", methods=["GET", "POST"])
+def delete(id):
+    Team.delete(id)
+    return jsonify({"status": "success"})
+
+
+@api.route("/update/<id>", methods=["GET", "POST"])
+def update(id):
+
+    team = Team.update("teamData")
+    return jsonify({"status": "success"})
